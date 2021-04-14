0a4fa1c8bf62027e3eee6f90a475aed16cf847d6
fanliang <franken133@163.com>
2021-04-14 22:23:25 +0800

    0414

--- src/pair.rs
+++ src/pair.rs
@@ -35,9 +35,62 @@ where
   P1: Parser<'a, R1>,
   P2: Parser<'a, R2>,
 {
-  map(pair(p1, p2), |(r1, _)|r1)
+  map(pair(p1, p2), |(r1, _)| r1)
   // let pair_fn = pair(p1, p2);
   // move |input| pair_fn.parse(input).map(|(s1, (r1, _))| (s1, r1))
+}
+
+fn right<'a, P1, P2, R1, R2>(p1: P1, p2: P2) -> impl Parser<'a, R2>
+where
+  P1: Parser<'a, R1>,
+  P2: Parser<'a, R2>,
+{
+  map(pair(p1, p2), |(_, r2)| r2)
+  // let pair_fn = pair(p1, p2);
+  // move |input| pair_fn.parse(input).map(|(s1, (r1, _))| (s1, r1))
+}
+
+fn one_or_more<'a, P, A>(p: P) -> impl Parser<'a, Vec<A>>
+where
+  P: Parser<'a, A>,
+{
+  move |mut input| {
+    let mut result = vec![];
+    match p.parse(input) {
+      Ok((s1, r1)) => {
+        input = s1;
+        result.push(r1);
+      }
+      Err(_) => {return Err(input)}
+    }
+    while let Ok((s1, r1)) = p.parse(input) {
+      input = s1;
+      result.push(r1);
+    }
+    Ok((input, result))
+  }
+}
+
+fn zero_or_more<'a, P, A>(p: P) -> impl Parser<'a, Vec<A>>
+where P: Parser<'a, A>
+{
+  move |mut input| {
+    let mut result = vec![];
+    while let Ok((s1, r1)) = p.parse(input) {
+      input = s1;
+      result.push(r1);
+    }
+    Ok((input, result))
+  }
+}
+
+#[test]
+fn test_right() {
+  let right_fn = right(match_literal("<"), identifier);
+  assert_eq!(
+    Ok(("/>", "I-am-test".to_string())),
+    right_fn.parse("<I-am-test/>")
+  )
 }
 
 #[test]
